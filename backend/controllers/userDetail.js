a2d73fb00f63c1059a95accbba91e4bb443c2fa7
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2025-07-02 09:45:08 +0530

    resolved issue of userName

--- backend/controllers/userDetail.js
+++ backend/controllers/userDetail.js
@@ -2,7 +2,8 @@ import UserModel from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 
 const userDetail=(req,res)=>{
-    const token=req.headers['authorization']?.split(' ')[1];
+    const token = req.cookies.token;
+    // const token=req.headers['authorization']?.split(' ')[1];
     const user=jwt.verify(token,process.env.SECRET);
     res.json(user.firstname);
 }
