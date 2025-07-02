a2bee48e041dabb34edce0d5b0274af856aa64e4
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2025-07-02 10:42:13 +0530

    pic photo not visible

--- backend/controllers/picphoto.js
+++ backend/controllers/picphoto.js
@@ -2,7 +2,9 @@ import PicModel from "../models/PicModel.js";
 import jwt from "jsonwebtoken";
 
 const picphoto=(req,res)=>{
-    const token=req.headers['authorization']?.split(' ')[1];
+    const token = req.cookies.token;
+    console.log(token);
+    // const token=req.headers['authorization']?.split(' ')[1];
     // console.log(token,"this is from picphoto")
     if (!token) return res.status(401).json({ message: 'Token missing' });
     const userdetail=jwt.verify(token,process.env.SECRET);
