efd9c64f3c914b93db40c43b1ae2d23e19feb89e
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2025-07-02 10:22:13 +0530

    context change

--- frontend/src/context/contexProvider.jsx
+++ frontend/src/context/contexProvider.jsx
@@ -2,17 +2,18 @@ import react, { createContext,useContext, useEffect, useState } from "react";
 import Cookie from "js-cookie";
 import axios from "axios";
 
+axios.defaults.withCredentials=true;
 const authContext=createContext();
 const ContexProvider=({children})=>{
 
     const [user,setUser]=useState(null);
+    const [isloggedin,setIsLoggedIn]=useState(false);
+    const token=Cookie.get("token");
+    // console.log(token)
     
     useEffect(()=>{
-        const token=Cookie.get("token");
-        if(!token){
-            return setUser('');
-        }
-        axios.get(`${import.meta.env.VITE_Server_URL}/userdetail`,{
+        if(isloggedin){
+            axios.get(`${import.meta.env.VITE_Server_URL}/userdetail`,{
             headers:{
                 Authorization:`Bearer ${token}`,
             }
@@ -22,12 +23,14 @@ const ContexProvider=({children})=>{
             setUser(result.data);
         })
         .catch(err=>{
+            setUser(null);
             console.log(err.message);
         })
-    },[]);
+        }
+    },[isloggedin]);
     
     return(
-        <authContext.Provider value={{user}}>{children}</authContext.Provider>
+        <authContext.Provider value={{user,setUser,setIsLoggedIn}}>{children}</authContext.Provider>
     )
 }
 
