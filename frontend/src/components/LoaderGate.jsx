216adfb5d1f49355b1fb2ef5498717e72e07a864
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2025-11-20 19:07:28 +0530

    added a coment

--- frontend/src/components/LoaderGate.jsx
+++ frontend/src/components/LoaderGate.jsx
@@ -1,6 +1,7 @@
 import { Loader } from "lucide-react";
 import { useAuth } from "../context/contexProvider";
 
+//loader for loading screen :-
 export default function LoaderGate({ children }) {
   const { isCheckingAuth } = useAuth();
 
